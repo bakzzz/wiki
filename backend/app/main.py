d34577ac320b1bc98f0bbb6f694c4dac710f8aa2
Денис Афанасьев <bakzzz@gmail.com>
2026-02-25 12:59:57 +0300

    fix(cors): explicitly set allow origins due to allow_credentials=True

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,13 @@ app = FastAPI(title="Wiki API")
 app.add_middleware(TenantMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost",
+        "https://wiki.chromaton.ru",
+        "http://wiki.chromaton.ru"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
